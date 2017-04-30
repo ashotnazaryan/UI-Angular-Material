1f24e40aed758a98782cd16483f21a95b48edf51
Ashot <ashotnazaryan45@outlook.com>
2017-04-30 12:16:42 +0400

    scss

--- GApp/src/app/components/shared/header/header.component.ts
+++ GApp/src/app/components/shared/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
