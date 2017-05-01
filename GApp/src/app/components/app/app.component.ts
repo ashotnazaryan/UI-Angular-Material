6ab34f399789b2e7c41ce932c8b02edeff00273c
Ashot <ashotnazaryan45@outlook.com>
2017-05-01 17:08:28 +0400

    h

--- GApp/src/app/components/app/app.component.ts
+++ GApp/src/app/components/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  @Input() sidenav: any;
+  constructor() {
+    
+  }
 }
--- GApp/src/app/components/shared/navmenu/navmenu.component.ts
+++ GApp/src/app/components/shared/navmenu/navmenu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-navmenu',
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class NavmenuComponent {
+    @Input() sidenav: any;
     user: Object;
     constructor(){
+        
         this.user = {
             firstName: "Ashot",
             lastName: "Nazaryan",
