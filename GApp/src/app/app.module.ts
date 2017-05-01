d0ac7498412960488d858d94038ddac4729482ca
Ashot <ashotnazaryan45@outlook.com>
2017-05-01 11:34:29 +0400

    commit

--- GApp/src/app/app.module.ts
+++ GApp/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
-
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './components/app/app.component';
-
+import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 
@@ -12,13 +12,14 @@ import { FooterComponent } from './components/shared/footer/footer.component';
     bootstrap: [AppComponent],
     declarations: [
         AppComponent,
-
+        HomeComponent,
         HeaderComponent,
         FooterComponent
     ],
     imports: [
         BrowserAnimationsModule,
-        MaterialModule     
+        MaterialModule,
+        AppRoutingModule
     ],
     providers: []
 })
