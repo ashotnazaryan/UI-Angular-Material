7106e4834030618c44c53598a0ea48c389901aa1
Ashot <ashotnazaryan45@outlook.com>
2017-05-01 14:55:34 +0400

    commit

--- GApp/src/app/components/shared/navmenu/navmenu.component.ts
+++ GApp/src/app/components/shared/navmenu/navmenu.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
 })
 
 export class NavmenuComponent {
-
+    user: Object;
+    constructor(){
+        this.user = {
+            firstName: "Ashot",
+            lastName: "Nazaryan",
+            location: "Yerevan",
+            about: "tra la la"
+        }
+    }
 }
