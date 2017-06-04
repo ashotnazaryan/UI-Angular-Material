41ce7d1d06070c14012866989b9bf98a4293a416
Ashot <ashotnazaryan45@outlook.com>
2017-06-04 22:30:54 +0400

    commit -h

--- GApp/public/src/app/pages/shared/header/header.ts
+++ GApp/public/src/app/pages/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
@@ -8,6 +8,8 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 })
 
  export class Header {
+
+    @Input() sidenav: any;
     currentLanguage: string;
     translate: TranslateService;
 
