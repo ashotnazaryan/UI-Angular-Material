93be0071d49f888342f0a8fdea6c70508294a3f8
Ashot <ashotnazaryan45@outlook.com>
2017-06-17 21:56:32 +0400

    commit -h

--- GApp/public/src/app/pages/about/about.ts
+++ GApp/public/src/app/pages/about/about.ts
@@ -17,7 +17,7 @@ export class About {
     }
     getNotes() {
         //'http://localhost:5000/api/notes';   http://gapp-def88.herokuapp.com/api/notes
-        let url = 'http://gapp-def88.herokuapp.com/api/notes';
+        let url = 'https://gapp-def88.herokuapp.com/api/notes';
         let params = {};
         this.loading = true;
         this.ts.getData(url, params)
